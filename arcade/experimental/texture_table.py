dfe99864aadb31b98dc0214a6aa564050d5bbfd6
pushfoo <36696816+pushfoo@users.noreply.github.com>
2021-04-20 12:18:11 -0400

    Move glyph display support to experimental

--- arcade/experimental/texture_table.py
+++ arcade/experimental/texture_table.py
@@ -7,8 +7,9 @@ these functions if you are an end user rather than a developer.
 """
 from pathlib import Path
 from typing import Union, List, Tuple, Dict
-from arcade import Texture, load_spritesheet
-
+from arcade import Texture, load_spritesheet, Color
+from arcade.color import WHITE
+from arcade.gui import text_utils
 
 TextureTable = Dict[str, Texture]
 
@@ -92,8 +93,8 @@ def load_monospace_spritesheet_font(
     count: int,
     margin: int = 0,
     glyph_selection: GlyphSelection = ASCII_WITH_LATIN_1,
-    check_lengths=True,
-    map_lower_to_upper=False
+    check_lengths: bool = True,
+    map_lower_to_upper: bool = False
 ) -> TextureTable:
     """
     Load a font from disk to a TextureTable (Dict[str, Texture]).
@@ -157,3 +158,92 @@ def load_monospace_spritesheet_font(
 
     return texture_table
 
+
+def render_glyph_texture_from_system_font(
+        glyph: str,
+        font_name: str = "arial",
+        font_size: int = 16,
+        font_color: Color = WHITE,
+) -> Texture:
+    """
+    Create a texture for a single character from a system font.
+
+    :param glyph: A 1-length string of any unicode codepoint.
+    :param font_name: what font to use.
+    :param font_size:
+    :param font_color:
+    :return:
+    """
+
+    if not isinstance(glyph, str):
+        raise TypeError(f"glyph must be a string, not {type(glyph)!r}")
+
+    if len(glyph) != 1:
+        raise ValueError("glyph string must be of length 1")
+
+    glyph_raw_image = text_utils.create_raw_text_image(
+        text=glyph,
+        font_name=font_name,
+        font_size=font_size,
+        font_color=font_color
+    )
+
+    # Ensure strange byte values are readable to humans. An early null
+    # byte in the texture name seems to cause issues in the interpreter
+    # and cause a blank stack trace to print on exit. Regardless, null
+    # is technically a valid unicode code point that can have map to
+    # glyphs, so we will try to sanitize all unprintable characters to
+    # readable names.
+    tex_name = f"{font_name}-{font_size}-{hex(ord(glyph))}"
+
+    glyph_texture = Texture(
+        tex_name,
+        glyph_raw_image,
+        # Ensure the dimensions of the image are used for packing to
+        # preserve kerning instead of making characters touch.
+        hit_box_algorithm='None'
+    )
+    return glyph_texture
+
+
+def build_glyph_table_from_system_font(
+        font_name: str = "arial",
+        font_size: int = 16,
+        font_color: Color = WHITE,
+        glyph_selection: GlyphSelection = ASCII_WITH_LATIN_1,
+        map_lower_to_upper: bool = False
+) -> Dict[str, Texture]:
+    """
+    Use a system font to build a table of glyph images to display data.
+
+    By default, attempt to rasterize glyphs for ASCII + latin1. This
+    includes null and other normally unprintable characters because
+    fonts can choose to provide glyphs for these character values even
+    if most do not.
+
+    Common use cases for these unusual fonts include shipping custom
+    glyphs for games and interfaces, so it makes sense to support this
+    feature here.
+
+    :param font_name: a string matching an installed system font
+    :param font_color: a color to set the font to
+    :param font_size: unclear what unit this is, may be points
+    :param glyph_selection: what glyphs to rasterize to the table
+    :return: A glyph table matching this font and size.
+    """
+
+    flattened_selection = flatten_glyph_selection(glyph_selection)
+
+    # build the glyph table for all included_glyphs in the passed range
+    glyph_table = {}
+
+    for char in flattened_selection:
+        glyph_table[char] = render_glyph_texture_from_system_font(
+            char,
+            font_name=font_name,
+            font_size=font_size,
+            font_color=font_color
+        )
+    return glyph_table
+
+DEFAULT_GLYPH_FONT = build_glyph_table_from_system_font()
